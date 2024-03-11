e69532ef7c5dd1901d59c247b55b23a427f4b7e4
vinay7391 <thakurvinay7391@gmail.com>
2024-03-12 00:11:34 +0530

    first code

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,sum ;
+cout<<"the value of a"<<endl;
+cin>>a;
+cout<<"the value of b"<<endl;
+cin>>b;
+ sum = a+b;
+
+cout<<"the sum of no.s"<<sum<<endl;
+
+    return 0;
+}
